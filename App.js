6da7674992a7d8210af8bebfb29f23b54c03cc0b
Josemi <jgallaso@localhost.localdomain>
2017-09-20 10:12:00 +0200

    initial commit for standalone apps

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>HELLO WORLD!!!</Text>
+        <Text>Powered by React-Native</Text>
       </View>
     );
   }
