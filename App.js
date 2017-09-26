6aa8294b96562cac0da48319876fcb70739f0524
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2017-09-26 16:53:34 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Alert, ToastAndroid, Platform } from 'react-native';
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>HELLO WORLD!!!</Text>
-        <Text>Powered by React-Native</Text>
+        <Text style={styles.title}>Hello World!!</Text>
+        <Text>Powered by React Native</Text>
+        <Button
+          title="Press me!"
+          style={styles.button}
+          onPress={this.onButtonPress} />
       </View>
     );
+  }
+
+  onButtonPress() {
+    if (Platform.OS === "ios") {
+      Alert.alert("Hola");
+    } else {
+      ToastAndroid.show("Hola", ToastAndroid.SHORT);
+    }
   }
 }
 
@@ -19,4 +31,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginBottom: 16
+  },
+  button: {
+
+  }
 });
